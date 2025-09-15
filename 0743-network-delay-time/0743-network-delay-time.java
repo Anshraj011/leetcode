6260daa9d495cf6e679854f5ee45e57e9a72853b
Anshraj011 <anshrajbiropur@gmail.com>
2025-09-15 16:47:02 +0530

    Time: 13 ms (49.43%), Space: 49.4 MB (17.03%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] time : times) {
+            graph.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[]{time[1], time[2]});
+        }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{k, 0});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int node = curr[0], time = curr[1];
+
+            if (!graph.containsKey(node)) continue;
+            for (int[] neighbor : graph.get(node)) {
+                int next = neighbor[0], travel = neighbor[1];
+                if (dist[next] > time + travel) {
+                    dist[next] = time + travel;
+                    pq.offer(new int[]{next, dist[next]});
+                }
+            }
+        }
+
+        int maxTime = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            maxTime = Math.max(maxTime, dist[i]);
+        }
+
+        return maxTime;
+    }
+}
